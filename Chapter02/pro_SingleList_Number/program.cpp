539924fa80b6c8bf711c50547d6023d76d121574
Nguyen Minh Phuc <nmphuc@outlook.com>
2021-09-23 16:47:23 +0700

    Thêm yêu cầu bài tập

--- Chapter02/pro_SingleList_Number/program.cpp
+++ Chapter02/pro_SingleList_Number/program.cpp
@@ -45,6 +45,10 @@ NODE* GetNode(int data)
 	p->link = NULL;
 	return p;
 }
+//Phương thức thực hiện thao tác thêm một node vào đầu danh sách.
+//Phương thức này có 2 tham số: 1 là danh sách kiểu LIST, 2 là một con trỏ NODE nắm giữ 1 NODE (node muốn thêm vào.
+//Có 2 trường học cần xử lý khi thêm 1 node vào đầu danh sách: 
+// TH1: Thêm vào danh sách rỗng (first = NULL
 void Addfirst(LIST& list, NODE* new_ele)
 {
 	if (list.first == NULL)
@@ -58,6 +62,10 @@ void Addfirst(LIST& list, NODE* new_ele)
 		list.first = new_ele;
 	}
 }
+//Phương thức thực hiện tác vụ thêm một giá trị data (sô nguyên) vào trong danh sách hiện có.
+//Thao tác này được thực hiện như sau:
+//Từ giá trị data được truyền từ tham số. Ta gọi hàm GetNode(data) để tạo một cấu trúc NODE cho danh sách liên kết đơn.
+//Sau khi đã tạo thành công NODE (node!=NULL) chúng ta tiến hành thêm node vừa tạo vào trong danh sách.
 void InsertList(LIST& list, int data)
 {
 	NODE* p;
@@ -67,6 +75,9 @@ void InsertList(LIST& list, int data)
 		Addfirst(list, p);
 	}
 }
+//Để việc thêm nhiều phần tử số nguyên vào trong danh sách (liên tục). Ta tiến hành xây dựng phương thức CreateList(list).
+//Phương thức này gồm 1 vòng lặp while chạy vô hạn, cho phép thực hiện việc nhập số nguyên liên tục, cho đến khi nào nhập giá trị -1 thì kết thúc.
+//Khi một số nguyên được nhập, Ta tiến hành gọi phưởng thưc InsertList để thêm giá trị đó vào danh sách liên kết.
 void CreateList(LIST& list)
 {
 	InitList(list);
@@ -81,7 +92,11 @@ void CreateList(LIST& list)
 		else { break; }
 	}
 }
-//Duyet danh sach
+//Để duyệt một danh sách liên kết ta thực hiện các bước sau:
+//1. khởi tạo 1 con trỏ kiểu NODE, để nắm giữ NODE đầu danh sách.
+//2. Cho vòng lặp chạy cho đến khi con trỏ p trỏ đến phần tử cuối cùng trong danh sách.
+//KHi thực hiện vòng lặp cần xác định 3 giá trị: giá trị khởi tạo (p=list.First), giá trị tăng (p=p->link), giá trị dừng (p!=NULL).
+//3. Xử lý node tại thời điểm hiện tại của con trỏ p.
 void PrintList(LIST list)
 {
 	NODE* p;
@@ -94,6 +109,18 @@ void PrintList(LIST list)
 		p = p->link;
 	}
 }
+//------------------------------Buổi học ngày 23-10-2021------------------------------
+//Thực hiện các thao tác sau:
+//1. In toàn bộ danh sách
+//2. In danh sách số chẵn trong danh sách
+//3. In danh sách số lẻ trong danh sách.
+//4. In danh sách các số nguyên tố có trong danh sách.
+//5. Tìm kiếm giá trị x nhập từ bàn phím
+//6. Tìm kiếm tất cả các giá trị x có trong danh sách. đếm xem có bao nhiêu giá trị x trong danh sách.
+//7. Xoá giá trị x khỏi danh sách.
+//8. Xoá tất cả các giá trị x khỏi danh sách.
+//9. xoá phần tử đầu danh sách.
+//10 xoá toàn bộ danh sách.
 void main()
 {
 	LIST list;
@@ -101,3 +128,5 @@ void main()
 	PrintList(list);
 	system("pause");
 }
+
+//Thực hiện bài tập này thêm
