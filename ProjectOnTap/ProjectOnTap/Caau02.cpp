9c7f6f846284492826a6398d2d4b80f09c543564
Nguyen Minh Phuc <nmphuc@outlook.com>
2022-01-07 20:35:30 +0700

    BaiTapOnTap

---
+++ ProjectOnTap/ProjectOnTap/Caau02.cpp
@@ -0,0 +1,119 @@
+﻿////-Mã hàng : chuỗi gồm tối đa 5 kí tự
+////- Tên hàng : chuỗi gồm tối đa 20 kí tự
+////- Số lượng tồn : số nguyên
+////- Đơn giá : số thực
+//
+//#include <iostream>
+//#include<string>
+//
+//using namespace std;
+//
+//typedef struct sanpham {
+//	string maSP;
+//	string tenSP;
+//	int soLuong;
+//	float dongia;
+//}SP;
+//
+//typedef struct node {
+//	SP data;
+//	node* left;
+//	node* right;
+//}NODE;
+//
+//typedef struct tree {
+//	NODE* root;
+//	int count;
+//}T;
+//
+//void Init(T& tree)
+//{
+//	tree.root = NULL;
+//	tree.count = 0;
+//}
+//NODE* GetNode(SP value)
+//{
+//	NODE* p = new NODE;
+//	if (p != NULL)
+//	{
+//		p->data = value;
+//		p->left = NULL;
+//		p->right = NULL;
+//	}
+//}
+//void InsertTree(NODE*& root, NODE* p)
+//{
+//	if (root == NULL)
+//		root = p;
+//	else {
+//		if (root->data.maSP > p->data.maSP)
+//		{
+//			InsertTree(root->left, p);
+//		}
+//		else if (root->data.maSP < p->data.maSP)
+//		{
+//			InsertTree(root->left, p);
+//		}
+//		else {
+//			cout << "node existed";
+//		}
+//	}
+//}
+//SP EnterProduct() {
+//	SP _sanPham;
+//	cin.ignore();
+//	cout << "MaSP: ";
+//	getline(cin, _sanPham.maSP);
+//	cout << "tenSP: ";
+//	getline(cin, _sanPham.tenSP);
+//	cout << "SL: "; cin >> _sanPham.soLuong;
+//	cout << "DG: "; cin >> _sanPham.dongia;
+//	return _sanPham;
+//}
+//void InputTree(NODE*& root)
+//{
+//	char option;
+//	SP _sanPham;
+//	while (true)
+//	{
+//		_sanPham = EnterProduct();
+//		NODE* p = GetNode(_sanPham);
+//		InsertTree(root, p);
+//		cout << " Option (y/n): "; cin >> option;
+//		if (option == 'n')
+//			break;
+//	}
+//}
+//void PrintSanPham(SP data)
+//{
+//	cout << data.maSP << "-" << data.tenSP << "-" << data.soLuong << "-" << data.dongia << "-" << "Thanh tien: " << data.soLuong * data.dongia;
+//}
+//void RNL(NODE* root) {
+//	if (root == NULL)
+//		return;
+//	RNL(root->right);
+//	PrintSanPham(root->data);
+//	RNL(root->left);
+//}
+//
+//float TinhTong(NODE* root) {
+//
+//	float tongTien = 0;
+//	if (root == NULL)
+//		return;
+//	return tongTien + TinhTong(root->right);
+//	tongTien = root->data.soLuong * root->data.dongia;
+//	return tongTien + TinhTong(root->left);
+//}
+//
+//void main()
+//{
+//	T tree;
+//	Init(tree);
+//	InputTree(tree.root);
+//	RNL(tree.root);
+//
+//	cout << TinhTong(tree.root);
+//
+//	system("pause");
+//}
---
+++ ProjectOnTap/ProjectOnTap/Cau01.cpp
@@ -0,0 +1,98 @@
+﻿////Định nghĩa và xây dựng DSLK đơn lưu trữ dãy gồm N ≤ 1000 số nguyên dương được nhập từ bàn phím.Lập trình đảo ngược thứ tự các phần tử của danh sách.Xuất DSLK đảo ngược ra màn hình.
+//#include <iostream>
+//using namespace std;
+//
+//typedef struct node {
+//	int data;
+//	node* link;
+//}NODE;
+//typedef struct list {
+//	NODE* first;
+//	NODE* last;
+//}LIST;
+//
+//void Init(LIST& list)
+//{
+//	list.first = list.last = NULL;
+//}
+//NODE* GetNode(int data)
+//{
+//	NODE* p = new NODE;
+//	if (p == NULL) {
+//		exit(1);
+//	}
+//	else {
+//		p->data = data;
+//		p->link = NULL;
+//	}
+//}
+//void AddFirst(LIST& list, NODE* newelement)
+//{
+//	if (list.first == NULL)
+//	{
+//		list.first = newelement;
+//		list.last = list.first;
+//	}
+//	else {
+//		newelement->link = list.first;
+//		list.first = newelement;
+//	}
+//}
+//void InsertList(LIST& list, int data)
+//{
+//	NODE* p = GetNode(data);
+//	if (p != NULL)
+//	{
+//		AddFirst(list, p);
+//	}
+//}
+//void CreateList(LIST& list) {
+//	Init(list);
+//	int data;
+//	while (true)
+//	{
+//		cout << "Input data: "; cin >> data;
+//		if (data != -1)
+//		{
+//			InsertList(list, data);
+//		}
+//		else {
+//			break;
+//		}
+//	}
+//}
+//
+//void PrintList(LIST list)
+//{
+//	NODE* p = list.first;
+//	while (p != NULL)
+//	{
+//		cout << p->data << " ";
+//		p = p->link;
+//	}
+//}
+//LIST DaoNguocDay(LIST listnguon) {
+//	LIST listResult;
+//	Init(listResult);
+//	NODE* p = listnguon.first;
+//
+//	while (p != NULL)
+//	{
+//		InsertList(listResult, p->data);
+//		p = p->link;
+//	}
+//
+//	return listResult;
+//}
+//void main()
+//{
+//	LIST list;
+//	LIST list2;
+//	CreateList(list);
+//
+//	PrintList(list);
+//	cout << endl;
+//	list2 = DaoNguocDay(list);
+//	PrintList(list2);
+//	system("pause");
+//}
